7bdb7026e0453b6d3b5fd2500814ef5b9568c140
OmkarPatil9 <70267513+OmkarPatil9@users.noreply.github.com>
2020-08-26 16:43:09 +0530

    Template1 site

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".btn").on('click', function () {
+    $(this).parent().toggleClass("showContent")
+
+
+    var replaceText = $(this).parent().hasClass("showContent") ? "Read Less" : "Read More";
+    $(this).text(replaceText);
+
+})
